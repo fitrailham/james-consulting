ee5489ab090b4938dcfb2cf007d035db0fce9923
Fitrailham <ilhamdoang123@gmail.com>
2021-01-27 17:38:02 +0700

    upadate latihan rabu 20210127

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'james-consulting';
-  
+
+  // // Initialize and add the map 
+  //  initMap(): void {
+  // // The location of Uluru
+  // const uluru = { lat: -25.344, lng: 131.036 };
+  // // The map, centered at Uluru
+  // const map = new google.maps.Map(
+  //   document.getElementById("map") as HTMLElement,
+  //   {
+  //     zoom: 4,
+  //     center: uluru,
+  //   }
+  // );
+
+  // // The marker, positioned at Uluru
+  // const marker = new google.maps.Marker({
+  //   position: uluru,
+  //   map: map,
+  // });
 }
+  
